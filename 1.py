78b6ffe1b3c1a29b81fe0ff8e081429d95ebdc13
Ashwini <ashwinee1031@gmail.com>
2026-09-17 14:16:56 +0530

    python

--- 1.py
+++ 1.py
@@ -1,1 +1,20 @@
 #create function
+#def greet():
+ #   print("hello")
+#greet()
+    
+#def pi():
+ #   return 3.14
+#print(pi())
+
+#arguments 
+def greet(name="student"):
+    print("hello",name)
+greet()
+greet("Yourname")
+    
+def square (n):
+    return n*n
+print(square(10))    
+    
+    
